ad1c768ffafb5e549c128ff51c7d514b9dad985f
Gabriel N. Schenker <gnschenker@gmail.com>
2016-05-08 18:20:23 -0500

    Added Dapper and DB bootstrap and access logic

---
+++ web-api/api/App_Start/Bootstrapper.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Data.SqlClient;
+using Dapper;
+
+namespace api
+{
+    public static class Bootstrapper
+    {
+        public static void Init()
+        {
+            var server = Db.ConnectionString = Environment.GetEnvironmentVariable("DB_SERVER") ?? @".\SQLEXPRESS2012";
+            var database = Db.ConnectionString = Environment.GetEnvironmentVariable("DB_DATABASE") ?? "Test";
+            var user = Db.ConnectionString = Environment.GetEnvironmentVariable("DB_USER_NAME") ?? "sa";
+            var pass = Db.ConnectionString = Environment.GetEnvironmentVariable("DB_PASSWORD") ?? "sa";
+            Db.ConnectionString = $"server={server};database=master;User Id={user};Password={pass};";
+            using (var conn = new SqlConnection(Db.ConnectionString))
+            {
+                var sql = $"IF db_id('{database}') is null create database [{database}];";
+                conn.Execute(sql);
+            }
+            Db.ConnectionString = $"server={server};database={database};User Id={user};Password={pass};";
+            using (var conn = new SqlConnection(Db.ConnectionString))
+            {
+                const string sql = @"
+                    if not exists (select * from sysobjects where name='Projects' and xtype='U')
+                    create table Projects(
+                      Id int identity not null,
+                      Name nvarchar(50) not null,
+                      constraint PK_Projects primary key(id)
+                    )";
+                conn.Execute(sql);
+            }
+        }
+    }
+
+    public static class Db
+    {
+        public static string ConnectionString { get; set; }
+    }
+}
--- web-api/api/Controllers/ProjectsController.cs
+++ web-api/api/Controllers/ProjectsController.cs
@@ -1,20 +1,41 @@
 ﻿using System.Collections.Generic;
 using System.Web.Http;
+using System.Data.SqlClient;
+using System.Linq;
+using Dapper;
 
 namespace api.Controllers
 {
     [Route("api/projects")]
     public class ProjectsController : ApiController
     {
+        private static string connString;
+
         [HttpGet]
         public IEnumerable<Project> Get()
         {
-            return new[]
+            using (var conn = new SqlConnection(Db.ConnectionString))
             {
-                new Project {id=1, name = "foo"},
-                new Project {id=2, name = "bar"},
-            };
-        } 
+                return conn.Query<Project>("SELECT id,name FROM Projects");
+            }
+        }
+
+        [HttpPost]
+        public int Post([FromBody] CreateProjectRequest req)
+        {
+            const string sql = 
+                @"INSERT INTO Projects(name) VALUES(@name);
+                  SELECT CAST(SCOPE_IDENTITY() as int)";
+            using (var conn = new SqlConnection(Db.ConnectionString))
+            {
+                return conn.Query<int>(sql, new {req.name}).Single();
+            }
+        }
+    }
+
+    public class CreateProjectRequest
+    {
+        public string name { get; set; }
     }
 
     public class Project
